44376fb040267c850ecf31d8de4bee6f8cfe552f
Maria <salyginamaria156@gmail.com>
2022-09-01 17:23:05 +0300

    fix: icons svg sprite

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use"); //обращение через класс .logo-svg к тегу svg (который внутри этого класса)
 window.addEventListener("scroll", () => {
    if(this.scrollY > 1 ){
-    navbar.classList.add("navbar-light")  //Добавляет класс под название navbar-light
+    navbar.classList.add("navbar-light");  //Добавляет класс под название navbar-light
+    logo.href.baseVal = "./img/sprite.svg#logo"
    }else{
-    navbar.classList.remove("navbar-light")
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "./img/sprite.svg#logo-light"
    }
     
-})
+});
