1fa20b7c8d1a168d163653b37a3cab2e39fc610c
Maria <salyginamaria156@gmail.com>
2022-09-07 19:28:41 +0300

    fix: adaptive and mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use"); //обращение через класс .logo-svg к тегу svg (который внутри этого класса)
+const mMenuToggle = document.querySelector(".mobile-menu-toggle")
+const menu = document.querySelector(".mobile-menu")
 window.addEventListener("scroll", () => {
    if(this.scrollY > 1 ){
     navbar.classList.add("navbar-light");  //Добавляет класс под название navbar-light
@@ -10,3 +12,8 @@ window.addEventListener("scroll", () => {
    }
     
 });
+
+mMenuToggle.addEventListener("click", (event)=>{
+   event.preventDefault();
+   menu.classList.toggle ("is-open")
+})
