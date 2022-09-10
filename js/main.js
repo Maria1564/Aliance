460aa7d3a84eab929359960ab5e10412443b077d
Maria <salyginamaria156@gmail.com>
2022-09-10 16:06:30 +0300

    add: sliders

--- js/main.js
+++ js/main.js
@@ -41,3 +41,55 @@ mMenuToggle.addEventListener("click", (event)=>{
    menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
+const swiper = new Swiper('.swiper', {
+   speed: 400,
+   autoHeight: true,
+   slidesPerView: 1,
+   navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 576px
+      576: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 1024px
+      1024: {
+        slidesPerView: 4,
+      },
+      // when window width is >= 1200px
+      1200: {
+         slidesPerView: 5,
+       }
+    }
+ });
+
+
+ const swiper_step = new Swiper('.swiper-step', {
+   speed: 400,
+   autoHeight: true,
+   slidesPerView: 1,
+   navigation: {
+      nextEl: '.slider-button-next_step',
+      prevEl: '.slider-button-prev_step',
+    },
+    breakpoints: {
+      // when window width is >= 576px
+    300: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 768px
+      760: {
+         slidesPerView: 3,
+      },
+
+      1100: {
+         slidesPerView:4,
+      }
+    }
+ });
