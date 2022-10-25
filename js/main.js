2b9ed90c2e16522a5060fc9fcb521a5ee6e86f2b
Maria <salyginamaria156@gmail.com>
2022-10-25 22:38:58 +0300

    add: about.html

--- js/main.js
+++ js/main.js
@@ -3,22 +3,23 @@ const logoLight = document.querySelector(".logo-light");
 const logo = document.querySelector(".logo"); 
 const mMenuToggle = document.querySelector(".mobile-menu-toggle")
 const menu = document.querySelector(".mobile-menu")
+const isFront = document.body.classList.contains("front-page") //содержит ли body класс front-page
 
-logo.style.display = "none"
 
 //Включает светлый режим меню
 const lightModeOn = (event)=>{
    navbar.classList.add("navbar-light"); //Добавляет класс под название navbar-light
-   logo.style.display = "block";
-   logoLight.style.display = "none";
 }
 
 //Выключает светлый режим меню
 const lightModeOff = (event)=>{
    navbar.classList.remove("navbar-light");
-   logo.style.display = "none";
-   logoLight.style.display = "block";
+}
 
+// Меняет высоту навигации
+const changeNavHeight = (height)=> {
+  navbar.style.height = height
+  console.log(height, navbar.style.height)
 }
 
 //Функция открывания меню
@@ -39,7 +40,10 @@ const closeMenu = (event)=>{
 }
 
 window.addEventListener("scroll", () => {
-   this.scrollY > 1 ? lightModeOn() : lightModeOff();
+   this.scrollY > 1 ? changeNavHeight("4.625rem") : changeNavHeight("5.875rem");
+   if(isFront) {
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
+   }
 });
 
 mMenuToggle.addEventListener("click", (event)=>{
